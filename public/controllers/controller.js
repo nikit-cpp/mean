a6b125dc918d1b391f82cc2385ca705d62b0c8ea
nik@fedora <nikit.cpp@yandex.ru>
2016-01-09 10:36:54 +0300

    last angular works

---
+++ public/controllers/controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nik on 08.01.16.
+ */
+
+
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('NikitaAppController', ['$scope', '$http',
+    function ($scope, $http){
+        console.log("HLLL frm ctrl");
+    }
+]);
+
