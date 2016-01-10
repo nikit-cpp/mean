9076bd7e8ea459e17c01555291cb5ab356569ad5
nik@fedora <nikit.cpp@yandex.ru>
2016-01-10 09:31:45 +0300

    basically json

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
 var express1 = require('express');
 var app = express1();
 
+var mongojs = require('mongojs');
+var db = mongojs('contactlistDb', ['contactlist']);
+
 app.use(express1.static(__dirname + '/public'));
 app.use(express1.static(__dirname + '/bower_components'));
 
 app.get('/contactlist2', function(req, res){
 	console.log("I received GET request");
 
-
-	var person1 = {
-		name: "Tttim",
-		email: "ololo@mail.ru",
-		number: "(123) 456-78-90"
-	};
-
-	var person2= {
-		name: "IVAN",
-		email: "ivan@mail.ru",
-		number: "(6665) 456-78-90"
-	};
-
-	var contactList = [person1, person2];
-
-	res.json(contactList);
+    db.contactlist.find(function(err, docs){
+        //console.log(docs);
+        res.json(docs);
+    });
 });
 
 var port = 8081;
