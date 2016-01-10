3182399df03847beca265861b611ff47b45ff37f
nik@fedora <nikit.cpp@yandex.ru>
2016-01-10 07:15:05 +0300

    lesson 2 savepoint

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -9,6 +9,24 @@ var myApp = angular.module('myApp', []);
 myApp.controller('NikitaAppController', ['$scope', '$http',
     function ($scope, $http){
         console.log("HLLL frm ctrl");
+		
+		$http.get('/contactlist2');
+		
+		var person1 = {
+			name: "Tttim",
+			email: "ololo@mail.ru",
+			number: "(123) 456-78-90"
+		};
+		
+		var person2= {
+			name: "IVAN",
+			email: "ivan@mail.ru",
+			number: "(6665) 456-78-90"
+		};
+		
+		var contactList = [person1, person2];
+		
+		$scope.contactList = contactList
     }
 ]);
 
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express1 = require('express');
 var app = express1();
 
-/*app.get('/', function(req, res){
-	res.send("H W frm njs1");
-})*/
-
 app.use(express1.static(__dirname + '/public'));
 app.use(express1.static(__dirname + '/bower_components'));
+
+app.get('/contactlist2', function(req, res){
+	console.log("I received request");
+})
 
 var port = 8081;
 app.listen(port);
