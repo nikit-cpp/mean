62e1740191b7db451c6f2a52703013821c7aefd6
nik@fedora <nikit.cpp@yandex.ru>
2016-01-10 08:03:17 +0300

    lesson 2 finished

--- server.js
+++ server.js
@@ -5,9 +5,26 @@ app.use(express1.static(__dirname + '/public'));
 app.use(express1.static(__dirname + '/bower_components'));
 
 app.get('/contactlist2', function(req, res){
-	console.log("I received request");
-})
+	console.log("I received GET request");
+
+
+	var person1 = {
+		name: "Tttim",
+		email: "ololo@mail.ru",
+		number: "(123) 456-78-90"
+	};
+
+	var person2= {
+		name: "IVAN",
+		email: "ivan@mail.ru",
+		number: "(6665) 456-78-90"
+	};
+
+	var contactList = [person1, person2];
+
+	res.json(contactList);
+});
 
 var port = 8081;
 app.listen(port);
-console.log("I a rnning at prt " + port);
+console.log("Server running on port " + port);
