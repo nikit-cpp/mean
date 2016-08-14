85286f3dc706b986e9737cdee3f2df6a95187c6a
nik@fedora <nikit.cpp@yandex.ru>
2016-08-15 01:49:38 +0300

    refreshing

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -9,24 +9,29 @@ var myApp = angular.module('myApp', []);
 myApp.controller('NikitaAppController', ['$scope', '$http',
 
     function ($scope, $http){
-        $scope.contact = {};
+
         console.log("That is controller.");
 
-        $http.get('/contactlist2').success(
-            function(response){
-                console.log('I got data what I requested');
-                $scope.contactList3 = response;
-            }
-        );
+        var refresh = function(){
+            $http.get('/contactlist2').success(
+                function(response){
+                    console.log('I got data what I requested');
+                    $scope.contactList3 = response;
+                    $scope.contact = {};
+                }
+            );
+        };
+
+        refresh();
 
         $scope.addContact = function(){
             console.log($scope.contact);
             $http.post('/contactlist2', $scope.contact).success(function(resp){
                 console.log(resp);
+                refresh();
             });
         };
 
     }
 
 ]);
-
